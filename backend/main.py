89477401ac3e54a6ec5a6a85560b77a4b426ad0b
Vasu Gadde <vasu.gadde@JHS.LOCAL>
2025-09-16 18:43:12 +0530

    Served frontend files as static

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from routes.upload_resume import upload_router
 from routes.match_resumes import match_router
 from routes.whatsapp_routes import whatsapp_router
 from routes.auth_routes import auth_router 
 from routes.all_candidates import candidate_router
+import os
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -22,6 +25,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Serve frontend folder
+frontend_path = os.path.join(os.path.dirname(__file__), "..", "frontend")
+app.mount("/static", StaticFiles(directory=frontend_path), name="static")
+
+# Serve index.html as root
 @app.get("/")
-def home():
-    return {"message": "Welcome to the Automated Recruitment System API"}
+def read_root():
+    return FileResponse(os.path.join(frontend_path, "index.html"))
+
+# Serve login.html
+@app.get("/login")
+def login_page():
+    return FileResponse(os.path.join(frontend_path, "login.html"))
+
+# Serve register.html
+@app.get("/register")
+def register_page():
+    return FileResponse(os.path.join(frontend_path, "register.html"))
